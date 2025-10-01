51a5dbe5bc9e53c6a09873f42793678f82258e10
benoy-beN <115520430+benoy-beN@users.noreply.github.com>
2025-10-01 16:17:34 +0530

    Update inference_gfpgan.py
    updated for CPU only processing
    line 61 change
    if not torch.cuda.is_available():
    To
    if False:
    in line 77 change
    half=True
    To
    half=False

--- inference_gfpgan.py
+++ inference_gfpgan.py
@@ -58,7 +58,7 @@ def main():
 
     # ------------------------ set up background upsampler ------------------------
     if args.bg_upsampler == 'realesrgan':
-        if not torch.cuda.is_available():  # CPU
+        if False:  # CPU
             import warnings
             warnings.warn('The unoptimized RealESRGAN is slow on CPU. We do not use it. '
                           'If you really want to use it, please modify the corresponding codes.')
@@ -74,7 +74,7 @@ def main():
                 tile=args.bg_tile,
                 tile_pad=10,
                 pre_pad=0,
-                half=True)  # need to set False in CPU mode
+                half=False)  # need to set False in CPU mode
     else:
         bg_upsampler = None
 
